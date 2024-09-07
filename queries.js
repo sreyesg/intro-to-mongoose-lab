db84f5df83dbc0e8198e29005f4030c1ef9c8cdb
sreyesg <santiagoreyes.g@gmail.com>
2024-09-07 12:42:09 -0700

    code that database connection and test

---
+++ queries.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const connect= ( async () => {
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('DB Connection openned')
+    await runQueries()
+    await mongoose.disconnect()
+    console.log('Connection closed')
+    
+})
+
+const runQueries = async () => {
+    
+    console.log('runing queries')
+}
+
+
+connect()
